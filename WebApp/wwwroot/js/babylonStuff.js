1f87a84d0eee9d97237bc530117075f717487061
gbull122 <gbull122@gmail.com>
2020-02-18 21:08:23 +0000

    Added new web App solution

---
+++ WebApp/wwwroot/js/babylonStuff.js
@@ -0,0 +1,13 @@
+﻿
+//var createScene = function () {
+//    var scene = new BABYLON.Scene(engine);
+//    scene.clearColor = new BABYLON.Color3(1, 0.8, 0.8);
+//};
+
+
+//var engine = new BABYLON.Engine(canvas, true);
+
+//var scene = createScene();
+
+//engine.runRenderLoop(function () {
+//    scene.render();
